e191665bbce700b0682a81517926cccdf0e1452d
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-05-07 20:16:50 +0700

    Menampilkan data kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return view('category');
+        $categories = Category::all();
+        return view('category', ['categories' => $categories]);
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,7 +3,31 @@
 @section('title', 'Category')
 
 @section('content')
-    <h1>
-        Category
-    </h1>
+    <h1>Category List</h1>
+
+    <div class="my-5">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($categories as $item)
+                
+                <tr>
+                  <th scope="row">{{$loop->iteration}}</th>
+                  <td><b>{{$item->name}}</b></td>
+                  <td>
+                    <a href="#"><button style="color: black">edit</button></a> 
+                    <a href="#"><button style="color: black">hapus</button></a>
+                  </td>
+                </tr>
+                    
+                @endforeach
+              </tbody>
+        </table>
+    </div>
 @endsection
