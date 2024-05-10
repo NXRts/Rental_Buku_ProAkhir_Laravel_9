f0e4c07d3b0969c81c4c0eb8f63358a07d131c29
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-05-10 09:01:30 +0700

    menampilkan data buku dari database

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index(){
-        return view('book');
+        $books = Book::all();
+        return view('book', ['books' => $books]);
     }
 }
--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -3,7 +3,31 @@
 @section('title', 'Books')
 
 @section('content')
-    <h1>
-       Books     
-    </h1>
+    <h1>Books List</h1>
+
+    <div class="my-5">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Code</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($books as $item)
+                <th scope="row">{{$loop->iteration}}</th>
+                <td>{{$item->book_code}}</td>
+                <td>{{$item->title}}</td>
+                <td>{{$item->status}}</td>
+                <td>
+                    <a href=""><button style="color: black">edit</button></a> 
+                    <a href=""><button style="color: black">hapus</button></a>
+                  </td>
+                @endforeach
+              </tbody>
+        </table>
+    </div>
 @endsection
