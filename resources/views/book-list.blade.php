db7ca367632285dd71def433f3de54ece8705cf7
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-05-14 17:55:03 +0700

    membuat desain tampilan halaman view publik

--- resources/views/book-list.blade.php
+++ resources/views/book-list.blade.php
@@ -3,5 +3,77 @@
 @section('title', 'Books List')
 
 @section('content')
-   
+
+    <h1>Books List</h1>
+    <hr>
+    <div class="my-5">
+        <div class="row">
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-3">
+                <div class="card">
+                    <img src="{{asset('images/icon_RB.png')}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">A32-6124</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p class="card-text text-end"> dasj</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
