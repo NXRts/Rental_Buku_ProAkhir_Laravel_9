4aad852b37ef9c685bd55a1c3c54ed1ea585b8d2
Muhammad Yusuf Arrofi <dangouu123@gmail.com>
2024-05-16 20:25:31 +0700

    membuat tampilan form peminjaman buku

--- resources/views/book-rent.blade.php
+++ resources/views/book-rent.blade.php
@@ -3,8 +3,21 @@
 @section('title', 'Book Rent')
 
 @section('content')
-    
-    <h1>Book Rent</h1>
-
-    
+    <div class="col-12 col-md-6 offset-md-2 col-lg-6 offset-md-3">
+        <h1>Book Rent</h1>
+        <hr class="mb-3">
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="user" class="form-label">User</label>
+                <select name="user" id="user" class="form-control"></select>
+            </div>
+            <div class="mb-3">
+                <label for="book" class="form-label">Book</label>
+                <select name="book" id="book" class="form-control"></select>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary w-100">Submit</button>
+            </div>
+        </form>
+    </div>
 @endsection
